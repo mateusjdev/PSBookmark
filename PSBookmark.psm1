1f4b60548ef02fe3a78d36abbf5cc288bdd1b556
mateusjdev <mateusjuniordev@gmail.com>
2024-12-22 17:23:47 -0300

    Change dataPath

--- PSBookmark.psm1
+++ PSBookmark.psm1
@@ -1,5 +1,5 @@
 ﻿#TODO: Don't like this method; will update this without breaking the module later
-$dataPath = "${env:USERPROFILE}\PSBookmarkData.ps1"
+$dataPath = "${env:USERPROFILE}\Documents\PSBookmarkData.ps1"
 
 #Credit: June Blender - https://www.sapien.com/blog/2014/10/21/a-better-tostring-method-for-hash-tables/
 function Convert-HashToString
